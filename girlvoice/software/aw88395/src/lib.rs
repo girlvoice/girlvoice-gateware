56a8d55c096d8ec0f8ff4690f02d5d0cf53c20c1
Sage Walker <cuby474@gmail.com>
2024-06-14 22:00:42 -0700

    A bunch of boilerplate for regmap in amp driver

--- girlvoice/software/aw88395/src/lib.rs
+++ girlvoice/software/aw88395/src/lib.rs
@@ -7,42 +7,29 @@
 
 // use embedded_hal::i2c;
 
+mod regmap;
+use regmap::{Aw88395Config, MappedRegister, Register};
+
 use embedded_hal::i2c::{Error, ErrorKind, I2c};
 
 
 const AW88395_ADDR: u8 = 0x34;
 
-enum Register {
-    Id = 0x00,
-    SysStat = 0x01,
-    SysInt = 0x02,
-    SysIntMask = 0x03,
-    SysCtrl = 0x04,
-    SysCtrl2 = 0x05,
-    I2SCtrl = 0x06,
-    I2SCfg1 = 0x07,
-    I2SCfg2 = 0x08,
-    HAGCCfg1 = 0x09,
-    HAGCCfg2 = 0x0a,
-    HAGCCfg3 = 0x0b,
-    VBat = 0x21,
-    Temp = 0x22,
-    PVdd = 0x23,
-    BstCtrl1 = 0x60,
-    BstCtrl2 = 0x61,
-}
 pub struct Aw88395<I2C> {
     i2c: I2C,
+    config: Aw88395Config,
 }
 
 pub enum Aw88395Error {
     OpFailed,
+    OutOfRange,
 }
 
 
 impl<I2C: I2c> Aw88395<I2C> {
     pub fn new(i2c: I2C) -> Self {
-        Self { i2c }
+        let config = Aw88395Config::default();
+        Self { i2c, config }
     }
 
     /// Consume the device and release the i2c device
@@ -51,13 +38,68 @@ impl<I2C: I2c> Aw88395<I2C> {
     }
 
     pub fn power_on(&mut self) -> Result<(), Aw88395Error> {
-        self.write_reg(Register::SysCtrl, 0x5306)?;
-        Ok(())
+        self.config.sys_ctrl.pwdn = false;
+        self.update_config(Register::SysCtrl)
+    }
+
+    pub fn power_off(&mut self) -> Result<(), Aw88395Error> {
+        self.config.sys_ctrl.pwdn = true;
+        self.update_config(Register::SysCtrl)
+    }
+
+    pub fn mute(&mut self) -> Result<(), Aw88395Error> {
+        self.config.sys_ctrl.mute = true;
+        self.update_config(Register::SysCtrl)
+    }
+
+    pub fn unmute(&mut self) -> Result<(), Aw88395Error> {
+        self.config.sys_ctrl.mute = false;
+        self.update_config(Register::SysCtrl)
+    }
+
+    pub fn enable_amp(&mut self) -> Result<(), Aw88395Error> {
+        self.config.sys_ctrl.amppd = false;
+        self.update_config(Register::SysCtrl)
+    }
+
+    pub fn disable_amp(&mut self) -> Result<(), Aw88395Error> {
+        self.config.sys_ctrl.amppd = true;
+        self.update_config(Register::SysCtrl)
+    }
+
+    pub fn set_volume(&mut self, volume: u16) -> Result<(), Aw88395Error> {
+        if volume > 0x3ff {
+            return Err(Aw88395Error::OutOfRange);
+        }
+        self.config.sys_ctrl2.vol_course = (volume >> 6) as u8;
+        self.config.sys_ctrl2.vol_fine = (volume & 0x3f) as u8;
+        self.update_config(Register::SysCtrl2)
+    }
+
+    fn update_config(&mut self, reg: Register) -> Result<(), Aw88395Error> {
+        let reg_val = self.config.reg_val(reg);
+        self.write_reg(reg, reg_val)
+    }
+
+    pub fn pll_locked(&mut self) -> Result<bool, Aw88395Error> {
+        self.update_status().map(|_| self.config.sys_stat.plls)
+    }
+
+    pub fn amp_switching(&mut self) -> Result<bool, Aw88395Error> {
+        self.update_status().map(|_| self.config.sys_stat.sws)
+    }
+
+    pub fn boost_init_finished(&mut self) -> Result<bool, Aw88395Error> {
+        self.update_status().map(|_| self.config.sys_stat.bsts)
+    }
+
+    fn update_status(&mut self) -> Result<(), Aw88395Error> {
+        self.read_reg(regmap::Register::SysStat).map(|val| self.config.sys_stat.update(val))
     }
 
     fn write_reg(&mut self, reg: Register, value: u16) -> Result<(), Aw88395Error> {
         let value_bytes = value.to_be_bytes();
-        let regbuf = [reg as u8, value_bytes[1], value_bytes[0]];
+        let regbuf = [reg.addr(), value_bytes[1], value_bytes[0]];
         match self.i2c.write(AW88395_ADDR, &regbuf) {
             Err(e) => match e.kind() {
                 ErrorKind::NoAcknowledge(_) => Err(Aw88395Error::OpFailed),
@@ -70,7 +112,7 @@ impl<I2C: I2c> Aw88395<I2C> {
     fn read_reg(&mut self, reg: Register) -> Result<u16, Aw88395Error> {
         let mut regbuf = [0u8; 2];
 
-        match self.i2c.write_read(AW88395_ADDR, &[reg as u8], &mut regbuf) {
+        match self.i2c.write_read(AW88395_ADDR, &[reg.addr()], &mut regbuf) {
             Err(e) => match e.kind() {
                 ErrorKind::NoAcknowledge(_) => Err(Aw88395Error::OpFailed),
                 _ => Err(Aw88395Error::OpFailed),
---
+++ girlvoice/software/aw88395/src/regmap.rs
@@ -0,0 +1,256 @@
+// Register map and defaults definitions
+#[derive(Copy, Clone)]
+pub enum Register {
+    Id = 0x00,
+    SysStat = 0x01,
+    SysInt = 0x02,
+    SysIntMask = 0x03,
+    SysCtrl = 0x04,
+    SysCtrl2 = 0x05,
+    I2SCtrl = 0x06,
+    I2SCfg1 = 0x07,
+    I2SCfg2 = 0x08,
+    HAGCCfg1 = 0x09,
+    HAGCCfg2 = 0x0a,
+    HAGCCfg3 = 0x0b,
+    VBat = 0x21,
+    Temp = 0x22,
+    PVdd = 0x23,
+    BstCtrl1 = 0x60,
+    BstCtrl2 = 0x61,
+}
+
+impl Register {
+    /// return register address as u8
+    pub fn addr(self) -> u8 {
+        self as u8
+    }
+}
+
+pub trait MappedRegister {
+    fn value(&self) -> u16;
+    fn update(&mut self, val: u16);
+}
+
+pub struct SysCtrl {
+    // From LSB to MSB
+    pub pwdn: bool,
+    pub amppd: bool,
+    pub dspby: bool,
+    pub ipll: bool,
+    pub bckinv: bool,
+    pub wsinv: bool,
+    pub i2sen: bool,
+    pub rcv_mode: bool,
+    pub mute: bool,
+    pub hdcce: bool,
+    pub hagce: bool,
+    pub rmse: bool,
+}
+
+pub struct SysCtrl2 {
+    intn: bool,
+    intmode: bool,
+    pub vol_fine: u8,
+    pub vol_course: u8,
+}
+
+impl MappedRegister for SysCtrl {
+    fn value(&self) -> u16 {
+        const RESERVED: u16 = 0x5;
+        let mut val: u16 = RESERVED;
+
+        val = (val << 1) | (self.rmse as u16);
+        val = (val << 1) | (self.hagce as u16);
+        val = (val << 1) | (self.hdcce as u16);
+        val = (val << 1) | (self.mute as u16);
+        val = (val << 1) | (self.rcv_mode as u16);
+        val = (val << 1) | (self.i2sen as u16);
+        val = (val << 1) | (self.wsinv as u16);
+        val = (val << 1) | (self.bckinv as u16);
+        val = (val << 1) | (self.ipll as u16);
+        val = (val << 1) | (self.dspby as u16);
+        val = (val << 1) | (self.amppd as u16);
+        val = (val << 1) | (self.pwdn as u16);
+
+        val
+    }
+
+    fn update(&mut self, val: u16) {
+        self.pwdn = (val & 1) == 1;
+        self.amppd = (val >> 1) & 1 == 1;
+        self.dspby = (val >> 2) & 1 == 1;
+        self.ipll = (val >> 3) & 1 == 1;
+        self.bckinv = (val >> 4) & 1 == 1;
+        self.wsinv = (val >> 5) & 1 == 1;
+        self.i2sen = (val >> 6) & 1 == 1;
+        self.rcv_mode = (val >> 7) & 1 == 1;
+        self.mute = (val >> 8) & 1 == 1;
+        self.hdcce = (val >> 9) & 1 == 1;
+        self.hagce = (val >> 10) & 1 == 1;
+        self.rmse = (val >> 11) & 1 == 1;
+    }
+}
+
+impl MappedRegister for SysCtrl2 {
+    fn value(&self) -> u16 {
+        const RESERVED: u16 = 0x9;
+        let mut val: u16 = self.vol_course as u16;
+        val = (val << 6) | (self.vol_fine as u16);
+        val = (val << 1) | (self.intmode as u16);
+        val = (val << 1) | (self.intn as u16);
+        val = (val << 4) | RESERVED;
+        val
+    }
+
+    fn update(&mut self, val: u16) {
+        self.intn = (val >> 4) & 1 == 1;
+        self.intmode = (val >> 5) & 1 == 1;
+        self.vol_fine = ((val >> 6) & 0x1f) as u8;
+        self.vol_course = ((val >> 12) & 0xf) as u8;
+    }
+}
+
+impl Default for SysCtrl {
+
+    fn default() -> Self {
+        Self {
+            pwdn: true,
+            amppd: true,
+            dspby: true,
+            ipll: false,
+            bckinv: false,
+            wsinv: false,
+            i2sen: false,
+            rcv_mode: false,
+            mute: true,
+            hdcce: true,
+            hagce: false,
+            rmse: false,
+        }
+    }
+}
+
+impl Default for SysCtrl2 {
+    fn default() -> Self {
+        Self {
+            intmode: false,
+            intn: false,
+            vol_fine: 0,
+            vol_course: 0,
+        }
+    }
+}
+#[readonly::make]
+pub struct SysStat {
+    pub plls: bool,
+    pub oths: bool,
+    pub clip_pres: bool,
+    pub ocds: bool,
+    pub clks: bool,
+    pub noclks: bool,
+    pub wds: bool,
+    pub clips: bool,
+    pub sws: bool,
+    pub bsts: bool,
+    pub ovps: bool,
+    pub bstocs: bool,
+    pub adps: bool,
+    pub uvls: bool,
+}
+
+impl MappedRegister for SysStat {
+    fn update(&mut self, val: u16) {
+        self.plls = (val & 1) == 1;
+        self.oths = (val >> 1) & 1 == 1;
+        self.clip_pres = (val >> 2) & 1 == 1;
+        self.ocds = (val >> 3) & 1 == 1;
+        self.clks = (val >> 4) & 1 == 1;
+        self.noclks = (val >> 5) & 1 == 1;
+        self.wds = (val >> 6) & 1 == 1;
+        self.clips = (val >> 7) & 1 == 1;
+        self.sws = (val >> 8) & 1 == 1;
+        self.bsts = (val >> 9) & 1 == 1;
+        self.ovps = (val >> 10) & 1 == 1;
+        self.bstocs = (val >> 11) & 1 == 1;
+        // Bit 12 Reserved
+        self.adps = (val >> 13) & 1 == 1;
+        self.uvls = (val >> 14) & 1 == 1;
+    }
+
+    fn value(&self) -> u16 {
+        let mut val: u16 = 0;
+        val = (val << 1) | self.uvls as u16;
+        val = (val << 1) | self.adps as u16;
+        val = (val << 1) | 0; // Reserved
+        val = (val << 1) | self.bstocs as u16;
+        val = (val << 1) | self.ovps as u16;
+        val = (val << 1) | self.bsts as u16;
+        val = (val << 1) | self.sws as u16;
+        val = (val << 1) | self.clips as u16;
+        val = (val << 1) | self.wds as u16;
+        val = (val << 1) | self.noclks as u16;
+        val = (val << 1) | self.clks as u16;
+        val = (val << 1) | self.ocds as u16;
+        val = (val << 1) | self.clip_pres as u16;
+        val = (val << 1) | self.oths as u16;
+        val = (val << 1) | self.plls as u16;
+        val
+    }
+}
+
+impl Default for SysStat {
+    fn default() -> Self {
+        Self {
+            plls: false,
+            oths: false,
+            clip_pres: false,
+            ocds: false,
+            clks: false,
+            noclks: false,
+            wds: false,
+            clips: false,
+            sws: false,
+            bsts: false,
+            ovps: false,
+            bstocs: false,
+            adps: false,
+            uvls: false,
+        }
+    }
+
+}
+
+pub struct Aw88395Config {
+    pub sys_stat: SysStat,
+    pub sys_ctrl: SysCtrl,
+    pub sys_ctrl2: SysCtrl2,
+}
+
+impl Default for Aw88395Config {
+    fn default() -> Self {
+        Self {
+            sys_stat: SysStat::default(),
+            sys_ctrl: SysCtrl::default(),
+            sys_ctrl2: SysCtrl2::default(),
+        }
+    }
+}
+
+impl Aw88395Config {
+    pub fn reg_val(&self, reg: Register) -> u16 {
+        match reg {
+            Register::SysCtrl => self.sys_ctrl.value(),
+            Register::SysCtrl2 => self.sys_ctrl2.value(),
+            _ => 0,
+        }
+    }
+
+    pub fn update_reg(&mut self, reg: Register, val: u16) {
+        match reg {
+            Register::SysCtrl => self.sys_ctrl.update(val),
+            Register::SysCtrl2 => self.sys_ctrl2.update(val),
+            _ => (),
+        }
+    }
+}
