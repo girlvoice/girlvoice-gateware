d44833dbf8e7cac4078a32fccbab660c7e8532a9
Sage Walker <cuby474@gmail.com>
2024-10-17 21:06:13 -0700

    Add sine sims

--- girlvoice/dsp/sine_synth.py
+++ girlvoice/dsp/sine_synth.py
@@ -1,9 +1,13 @@
+import os
 import numpy as np
+import matplotlib.pyplot as plt
 from amaranth import *
 import amaranth.lib.wiring as wiring
 from amaranth.lib.wiring import In, Out
 from amaranth.lib import stream
+from amaranth.sim import Simulator
 
+from girlvoice.stream import stream_get
 class StaticSineSynth(wiring.Component):
 
     def __init__(self, frequency, fs, sample_width):
@@ -29,8 +33,9 @@ class StaticSineSynth(wiring.Component):
 
         t = np.linspace(0, 2*np.pi, N)
         y = np.sin(t)
-        y_norm = y * (2**self.sample_width)
+        y_norm = y * (2**(self.sample_width - 1))
         print(t)
+        print(y)
         print(y_norm)
         ring = Array([C(int(y_i), signed(self.sample_width)) for y_i in y_norm])
 
@@ -46,4 +51,46 @@ class StaticSineSynth(wiring.Component):
 
         return m
 
+def run_sim():
+    clk_freq = 60e6
+    bit_width = 16
+    target_freq = 1000
+    fs = 48000
+    duration = 1
+    dut = StaticSineSynth(frequency=target_freq, fs=fs, sample_width=bit_width)
+
+    num_samples = fs * duration
+
+    t = np.linspace(0, duration, num_samples)
+    output_samples = []
+    async def tb(ctx):
+        ctx.set(dut.en, 1)
+        for _ in range(num_samples):
+            output_samples.append(await stream_get(ctx, dut.source))
+            await ctx.tick()
+
+    sim = Simulator(dut)
+    sim.add_clock(1/clk_freq)
+    sim.add_testbench(tb)
+
+    os.makedirs("gtkw", exist_ok=True)
+    dutname = f"gtkw/{type(dut).__name__}"
+    with sim.write_vcd(dutname + f".vcd"):
+        sim.run()
+        # bode_plot(fs, duration, end_freq, input_samples, output_samples)
+        ax2 = plt.subplot(111)
+        ax2.plot(t, output_samples, alpha=0.5, label="Output")
+        ax2.set_xlabel('time (s)')
+        plt.title('Sine Synth')
+        plt.grid(True)
+        plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
+        plt.legend()
+        # plt.savefig(f"{type(dut).__name__}.png")
+        plt.show()
+
+
+
+
+if __name__ == "__main__":
+    run_sim()
 
--- girlvoice/dsp/utils.py
+++ girlvoice/dsp/utils.py
@@ -3,16 +3,16 @@ import numpy as np
 import scipy.signal as signal
 import matplotlib.pyplot as plt
 
-def generate_chirp(duration, fs, start_freq, end_freq, sample_width):
+def generate_chirp(duration, fs, start_freq, end_freq, sample_width, amp=1.0):
     num_samples = duration * fs
     t = np.linspace(0, duration, num_samples)
 
-    x = signal.chirp(t, f0 = start_freq, f1=end_freq, t1=duration)
+    x = amp * signal.chirp(t, f0 = start_freq, f1=end_freq, t1=duration)
     x = x * signal.windows.tukey(num_samples, 0.05)
     x *= 2**(sample_width-1)
     return (t, x)
 
-def bode_plot(fs, duration, end_freq, input, output, taps):
+def bode_plot(fs, duration, end_freq, input, output, taps, taps_quant):
     fft_out = np.fft.fft(output)
 
     freq = np.linspace(1, end_freq, (fs*duration)//2)
@@ -24,6 +24,7 @@ def bode_plot(fs, duration, end_freq, input, output, taps):
     gain = 20 * np.log10(np.abs(resp))
 
     w_ideal, h_ideal = signal.freqz(b=taps[0], a=taps[1], worN=freq, fs=fs)
+    w_q, h_q = signal.freqz(b=taps_quant[0], a=taps_quant[1], worN=freq, fs=fs)
     #phase = np.angle(h, deg=True)
     subplt = plt.subplot(122)
     subplt.plot(w_ideal, gain, label="Simulated Gain")
@@ -32,3 +33,4 @@ def bode_plot(fs, duration, end_freq, input, output, taps):
     subplt.set_ylabel("Gain (dB)")
 
     subplt.plot(w_ideal, 20*np.log10(np.abs(h_ideal)), label="Ideal Gain")
+    subplt.plot(w_q, 20*np.log10(np.abs(h_q)), label="Quantized Coeff Gain")
