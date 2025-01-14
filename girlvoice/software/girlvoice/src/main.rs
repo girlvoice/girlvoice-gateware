53f1476097ea88fcc920f08e333dbc0e32d0755f
Sage Walker <cuby474@gmail.com>
2025-01-13 22:34:52 -0800

    Refactor Litex PAC for embedded-hal 1.0. Start playing with LCD driver

--- girlvoice/software/girlvoice/src/main.rs
+++ girlvoice/software/girlvoice/src/main.rs
@@ -1,58 +1,124 @@
 #![no_std]
 #![no_main]
 
-use embedded_hal::i2c::{I2c, SevenBitAddress};
-use hex;
-use hal::hal::blocking::serial::Write;
+use embedded_hal::{digital::OutputPin, i2c::{I2c, SevenBitAddress}};
+// use embedded_io::Write;
+use gc9a01::{mode::DisplayConfiguration, prelude::DisplayResolution240x240, Gc9a01, SPIDisplayInterface};
+// use hex;
 use litex_pac as pac;
-use aw88395::Aw88395;
+// use aw88395::Aw88395;
 use riscv_rt::entry;
 extern crate panic_halt;
 
+use embedded_graphics::{
+    pixelcolor::Rgb565,
+    prelude::*,
+    primitives::{
+        PrimitiveStyle, Triangle,
+    },
+};
+
 mod timer;
 mod i2c;
+mod spi;
 
-use i2c::I2c0;
-use timer::Timer;
+// use i2c::I2c0;
+
 use litex_hal as hal;
 
 const SYS_CLK_FREQ: u32 = 60_000_000;
 
-hal::uart! {
-    UART: pac::Uart,
+// hal::uart! {
+//     UART: pac::Uart,
+// }
+
+hal::gpio! {
+    DC: pac::LcdCtl,
+}
+hal::gpio! {
+    BL: pac::LcdBl,
+}
+
+hal::spi! {
+    SPI: (pac::LcdSpi, u8),
+}
+
+hal::timer! {
+    DELAY: pac::Timer0,
 }
 
 
 #[entry]
 fn main() -> ! {
     let peripherals = pac::Peripherals::take().unwrap();
-    let mut timer = Timer::new(peripherals.timer0);
 
-    let mut serial = UART {
-        registers: peripherals.uart,
+    // let mut serial = UART {
+    //     registers: peripherals.uart,
+    // };
+
+    let lcd_spi = SPI{
+        registers: peripherals.lcd_spi
     };
 
+    let mut delay = DELAY {
+        registers: peripherals.timer0,
+        sys_clk: SYS_CLK_FREQ
+    };
+
+    let dc = DC { index: 0 }; // LCD Data/Command GPIO
+    let mut bl = BL { index: 0 }; // LCD Backlight GPIO
+
+    bl.set_high().unwrap();
+
+    let interface = SPIDisplayInterface::new(lcd_spi, dc);
+
     // let i2c_freq = HertzU32::from_raw(400_000);
-    let mut i2c0 = I2c0::new(peripherals.i2cfifo);
+    // let mut i2c0 = I2c0::new(peripherals.i2cfifo);
+
+    let mut display_driver = Gc9a01::new(
+        interface,
+        DisplayResolution240x240,
+        gc9a01::prelude::DisplayRotation::Rotate0
+    );
+
+    display_driver.init(&mut delay).unwrap();
 
 
-    serial.bwrite_all(b"Starting I2C read!\n").unwrap();
+    // display_driver.set_invert_pixels(true).unwrap();
+
+    // display_driver.clear_fit().unwrap();
+
+    // embedded-graphics hello world
+
+    // Triangle::new(Point::new(32, 16), Point::new(16, 48), Point::new(48, 48))
+    // .into_styled(PrimitiveStyle::with_stroke(Rgb565::MAGENTA, 1))
+    // .draw(&mut display_driver).unwrap();
+
+    let pix: [u16; 100] = [Rgb565::MAGENTA.into_storage(); 100];
+
+    display_driver.set_pixels((100, 100), (110, 110), &mut pix.into_iter()).unwrap();
+
+    loop {}
+
+    // serial.write_all(b"Starting I2C read!\n").unwrap();
 
     // let test = TxCmd::RestartCount.value();
     // let mut test_hex = [0_u8; 2];
     // hex::encode_to_slice(test.to_be_bytes(), &mut test_hex).unwrap();
-    // serial.bwrite_all(b"0x").unwrap();
-    // serial.bwrite_all(&test_hex).unwrap();
-    // serial.bwrite_all(b"\n").unwrap();
+    // serial.write_all(b"0x").unwrap();
+    // serial.write_all(&test_hex).unwrap();
+    // serial.write_all(b"\n").unwrap();
 
-    let dev_addr: SevenBitAddress = 0x34;
-    let max_reg_addr: u8 = 0x61;
-    const BYTES_TO_READ: usize = 2;
-    const REG_WIDTH: usize = 1;
-    let mut read_buf = [0_u8; BYTES_TO_READ];
-    let mut hex_bytes = [0_u8; BYTES_TO_READ * 2];
+    // let dev_addr: SevenBitAddress = 0x34;
+    // let max_reg_addr: u8 = 0x61;
+    // const BYTES_TO_READ: usize = 2;
+    // const REG_WIDTH: usize = 1;
+    // let mut read_buf = [0_u8; BYTES_TO_READ];
+    // let mut hex_bytes = [0_u8; BYTES_TO_READ * 2];
 
-    let mut reg_hex = [0_u8; REG_WIDTH * 2];
+    // let mut reg_hex = [0_u8; REG_WIDTH * 2];
+
+
 
     // i2c0.write(dev_addr, &[0x00, 0x55, 0xaa]).unwrap();
 
@@ -60,17 +126,17 @@ fn main() -> ! {
     // for reg_addr in (0..max_reg_addr+1).step_by(1) {
 
     //     hex::encode_to_slice(reg_addr.to_be_bytes(), &mut reg_hex).unwrap();
-    //     serial.bwrite_all(b"0x").unwrap();
-    //     serial.bwrite_all(&reg_hex).unwrap();
-    //     serial.bwrite_all(b": ").unwrap();
+    //     serial.write_all(b"0x").unwrap();
+    //     serial.write_all(&reg_hex).unwrap();
+    //     serial.write_all(b": ").unwrap();
 
     //     i2c0.write_read(dev_addr, &reg_addr.to_be_bytes(), &mut read_buf).unwrap();
 
-    //     serial.bwrite_all(b"0x").unwrap();
+    //     serial.write_all(b"0x").unwrap();
     //     hex::encode_to_slice(read_buf, &mut hex_bytes).unwrap();
 
-    //     serial.bwrite_all(&hex_bytes).unwrap();
-    //     serial.bwrite_all(b"\n").unwrap();
+    //     serial.write_all(&hex_bytes).unwrap();
+    //     serial.write_all(b"\n").unwrap();
 
     //     msleep(&mut timer, 1)
     // }
@@ -85,114 +151,102 @@ fn main() -> ! {
 
 
     // hex::encode_to_slice(reg_addr.to_be_bytes(), &mut reg_hex).unwrap();
-    // serial.bwrite_all(b"0x").unwrap();
-    // serial.bwrite_all(&reg_hex).unwrap();
-    // serial.bwrite_all(b": ").unwrap();
+    // serial.write_all(b"0x").unwrap();
+    // serial.write_all(&reg_hex).unwrap();
+    // serial.write_all(b": ").unwrap();
 
     // i2c0.write_read(dev_addr, &reg_addr.to_be_bytes(), &mut read_buf).unwrap();
     // let mut config = amp.get_sysctrl_bits().unwrap();
     // hex::encode_to_slice(config.to_be_bytes(), &mut config_hex).unwrap();
-    // serial.bwrite_all(b"0x").unwrap();
-    // serial.bwrite_all(&config_hex).unwrap();
-    // serial.bwrite_all(b"\n").unwrap();
+    // serial.write_all(b"0x").unwrap();
+    // serial.write_all(&config_hex).unwrap();
+    // serial.write_all(b"\n").unwrap();
 
-    // serial.bwrite_all(b"0x").unwrap();
+    // serial.write_all(b"0x").unwrap();
     // hex::encode_to_slice(read_buf, &mut hex_bytes).unwrap();
 
-    // serial.bwrite_all(&hex_bytes).unwrap();
-    // serial.bwrite_all(b"\n").unwrap();
+    // serial.write_all(&hex_bytes).unwrap();
+    // serial.write_all(b"\n").unwrap();
 
-    let mut amp = Aw88395::new(i2c0);
+    // let mut amp = Aw88395::new(i2c0);
 
-    let mut config_hex = [0u8; 4];
+    // let mut config_hex = [0u8; 4];
 
-    amp.soft_reset().unwrap();
+    // amp.soft_reset().unwrap();
 
-    let mut config = amp.get_sysctrl_bits().unwrap();
-    hex::encode_to_slice(config.to_be_bytes(), &mut config_hex).unwrap();
-    serial.bwrite_all(b"0x").unwrap();
-    serial.bwrite_all(&config_hex).unwrap();
-    serial.bwrite_all(b"\n").unwrap();
+    // let mut config = amp.get_sysctrl_bits().unwrap();
+    // hex::encode_to_slice(config.to_be_bytes(), &mut config_hex).unwrap();
+    // serial.write_all(b"0x").unwrap();
+    // serial.write_all(&config_hex).unwrap();
+    // serial.write_all(b"\n").unwrap();
 
-    serial.bwrite_all(b"Starting amp power-up sequence\n").unwrap();
-    serial.bwrite_all(b"Enabling I2S Interface\n").unwrap();
-    serial.bwrite_all(b"Powering on\n").unwrap();
-    amp.power_on().unwrap();
-    serial.bwrite_all(b"Power on success\n").unwrap();
+    // serial.write_all(b"Starting amp power-up sequence\n").unwrap();
+    // serial.write_all(b"Enabling I2S Interface\n").unwrap();
+    // serial.write_all(b"Powering on\n").unwrap();
+    // amp.power_on().unwrap();
+    // serial.write_all(b"Power on success\n").unwrap();
 
-    msleep(&mut timer, 1000);
+    // msleep(&mut timer, 1000);
 
-    config = amp.get_sysctrl_bits().unwrap();
-    hex::encode_to_slice(config.to_be_bytes(), &mut config_hex).unwrap();
-    serial.bwrite_all(b"0x").unwrap();
-    serial.bwrite_all(&config_hex).unwrap();
-    serial.bwrite_all(b"\n").unwrap();
+    // config = amp.get_sysctrl_bits().unwrap();
+    // hex::encode_to_slice(config.to_be_bytes(), &mut config_hex).unwrap();
+    // serial.write_all(b"0x").unwrap();
+    // serial.write_all(&config_hex).unwrap();
+    // serial.write_all(b"\n").unwrap();
 
 
-    while !amp.pll_locked().unwrap() {
-        serial.bwrite_all(b"Waiting for PLL lock...\n").unwrap();
-        msleep(&mut timer, 2000);
-    }
-    serial.bwrite_all(b"PLL Locked\n").unwrap();
+    // while !amp.pll_locked().unwrap() {
+    //     serial.write_all(b"Waiting for PLL lock...\n").unwrap();
+    //     msleep(&mut timer, 2000);
+    // }
+    // serial.write_all(b"PLL Locked\n").unwrap();
 
-    serial.bwrite_all(b"Setting volume to 500 i guess\n").unwrap();
-    amp.set_volume(400).unwrap();
-    // amp.set_i2s_channel(aw88395::ChannelSetting::Left).unwrap();
-    amp.enable_i2s().unwrap();
-    amp.set_i2s_data_fmt(aw88395::I2SDataFmt::MSBFirst).unwrap();
-    amp.set_i2s_samplerate(0x6).unwrap();
-    amp.set_i2s_data_width(0x0).unwrap();
+    // serial.write_all(b"Setting volume to 500 i guess\n").unwrap();
+    // amp.set_volume(400).unwrap();
+    // // amp.set_i2s_channel(aw88395::ChannelSetting::Left).unwrap();
+    // amp.enable_i2s().unwrap();
+    // amp.set_i2s_data_fmt(aw88395::I2SDataFmt::MSBFirst).unwrap();
+    // amp.set_i2s_samplerate(0x6).unwrap();
+    // amp.set_i2s_data_width(0x0).unwrap();
 
-    serial.bwrite_all(b"Enabling class D amplifier and boost converter\n").unwrap();
-    amp.enable_amp().unwrap();
-    serial.bwrite_all(b"Waiting for amplifier power-on...\n").unwrap();
+    // serial.write_all(b"Enabling class D amplifier and boost converter\n").unwrap();
+    // amp.enable_amp().unwrap();
+    // serial.write_all(b"Waiting for amplifier power-on...\n").unwrap();
 
-    while !amp.boost_init_finished().unwrap() {
-        serial.bwrite_all(b"Waiting for amplifier power-on...\n").unwrap();
-        msleep(&mut timer, 2000);
-    }
-    serial.bwrite_all(b"Amplifier power-on complete\n").unwrap();
+    // while !amp.boost_init_finished().unwrap() {
+    //     serial.write_all(b"Waiting for amplifier power-on...\n").unwrap();
+    //     msleep(&mut timer, 2000);
+    // }
+    // serial.write_all(b"Amplifier power-on complete\n").unwrap();
 
-    serial.bwrite_all(b"Unmuting\n").unwrap();
-    amp.unmute().unwrap();
+    // serial.write_all(b"Unmuting\n").unwrap();
+    // amp.unmute().unwrap();
 
-    i2c0 = amp.release();
+    // i2c0 = amp.release();
 
-    for reg_addr in (0..max_reg_addr+1).step_by(1) {
+    // for reg_addr in (0..max_reg_addr+1).step_by(1) {
 
-        hex::encode_to_slice(reg_addr.to_be_bytes(), &mut reg_hex).unwrap();
-        serial.bwrite_all(b"0x").unwrap();
-        serial.bwrite_all(&reg_hex).unwrap();
-        serial.bwrite_all(b": ").unwrap();
+    //     hex::encode_to_slice(reg_addr.to_be_bytes(), &mut reg_hex).unwrap();
+    //     serial.write_all(b"0x").unwrap();
+    //     serial.write_all(&reg_hex).unwrap();
+    //     serial.write_all(b": ").unwrap();
 
-        i2c0.write_read(dev_addr, &reg_addr.to_be_bytes(), &mut read_buf).unwrap();
+    //     i2c0.write_read(dev_addr, &reg_addr.to_be_bytes(), &mut read_buf).unwrap();
 
-        serial.bwrite_all(b"0x").unwrap();
-        hex::encode_to_slice(read_buf, &mut hex_bytes).unwrap();
+    //     serial.write_all(b"0x").unwrap();
+    //     hex::encode_to_slice(read_buf, &mut hex_bytes).unwrap();
 
-        serial.bwrite_all(&hex_bytes).unwrap();
-        serial.bwrite_all(b"\n").unwrap();
+    //     serial.write_all(&hex_bytes).unwrap();
+    //     serial.write_all(b"\n").unwrap();
 
-        msleep(&mut timer, 1)
-    }
-    serial.bwrite_all(b"Welcome to girlvoice\n").unwrap();
-    loop {
-        serial.bwrite_all(b"awaw").unwrap();
-        // led.toggle();
-        // i2c0.setup_bus(8, true);
-        msleep(&mut timer, 500);
-        // i2c0.clear();
-    }
-}
-
-fn msleep(timer: &mut Timer, ms: u32) {
-    timer.disable();
-
-    timer.reload(0);
-    timer.load(SYS_CLK_FREQ / 1_000 * ms);
-
-    timer.enable();
-
-    // Wait until the time has elapsed
-    while timer.value() > 0 {}
+    //     msleep(&mut timer, 1)
+    // }
+    // serial.write_all(b"Welcome to girlvoice\n").unwrap();
+    // loop {
+    //     serial.write_all(b"awaw").unwrap();
+    //     // led.toggle();
+    //     // i2c0.setup_bus(8, true);
+    //     msleep(&mut timer, 500);
+    //     // i2c0.clear();
+    // }
 }
--- girlvoice/software/girlvoice/src/spi.rs
+++ girlvoice/software/girlvoice/src/spi.rs
@@ -4,9 +4,30 @@ macro_rules! spi {
         $SPIX:ident: ($PACSPIX:ty, $WORD:ty),
     )+) => {
         $(
+
             #[derive(Debug)]
             pub struct $SPIX {
-                registers: $PACSPIX,
+                registers: $PACSPIX
+            }
+
+            #[derive(Debug, Copy, Clone, Eq, PartialEq)]
+            pub enum Error {
+                TransactionFailed,
+                InvalidState,
+                Failed,
+            }
+            impl $crate::hal::spi::Error for Error {
+                fn kind(&self) -> hal::spi::ErrorKind {
+                    match *self {
+                        Error::TransactionFailed => $crate::hal::spi::ErrorKind::Other,
+                        Error::InvalidState => $crate::hal::spi::ErrorKind::Other,
+                        Error::Failed => $crate::hal::spi::ErrorKind::Other,
+                    }
+                }
+            }
+
+            impl hal::spi::ErrorType for $SPIX {
+                type Error = Error;
             }
 
             impl $SPIX {
@@ -17,42 +38,111 @@ macro_rules! spi {
                 pub fn free(self) -> $PACSPIX {
                     self.registers
                 }
-            }
 
-            impl $crate::hal::spi::FullDuplex<$WORD> for $SPIX {
-                type Error = core::convert::Infallible;
-
-                fn read(&mut self) -> $crate::nb::Result<$WORD, Self::Error> {
-                    if self.registers.status.read().done().bit() {
-                        Ok(self.registers.miso.read().bits() as $WORD)
-                    } else {
-                        Err($crate::nb::Error::WouldBlock)
-                    }
-                }
-
-                fn send(&mut self, word: u8) -> $crate::nb::Result<(), Self::Error> {
-                    if self.registers.status.read().done().bit() {
+                fn write_one(&mut self, word: &u8) -> Result<(), Error> {
+                    if self.registers.status().read().done().bit() {
                         unsafe {
-                            self.registers.mosi.write(|w| w.bits(word.into()));
-                            self.registers.control.write(|w| {
+                            self.registers.mosi().write(|w| w.bits(word.into()));
+                            self.registers.control().write(|w| {
                                 w.length().bits(8).start().bit(true)
                             });
                         }
                         Ok(())
                     } else {
-                        Err($crate::nb::Error::WouldBlock)
+                        Err(Error::InvalidState)
+                    }
+                }
+
+                fn is_done(&mut self) -> bool {
+                    self.registers.status().read().done().bit()
+                }
+
+                fn read_priv(&mut self, bufs: &mut [$WORD]) ->  Result<(), Error> {
+                    if self.registers.status().read().done().bit() {
+                        for buf in bufs.iter_mut() {
+                            unsafe {
+                                self.registers.control().write(|w| {
+                                    w.length().bits(8).start().bit(true)
+                                });
+                            }
+                            while !self.is_done() {}
+                            *buf = self.registers.miso().read().bits() as u8;
+                        }
+                        Ok(())
+                    } else {
+                        Err(Error::InvalidState)
+                    }
+                }
+
+                fn write_priv(&mut self, words: &[u8]) -> Result<(), Error> {
+                    if self.registers.status().read().done().bit() {
+                        for word in words.iter() {
+                            self.write_one(word);
+                            while !self.is_done() {}
+                        }
+                        Ok(())
+                    } else {
+                        Err(Error::InvalidState)
+                    }
+                }
+
+                fn transfer_priv(&mut self, read: &mut [$WORD], write: &[$WORD]) -> Result<(), Error> {
+                    let len = read.len().max(write.len());
+                    for i in 0..len {
+                        let wb = write.get(i).copied().unwrap_or(0);
+
+                        self.write_one(&wb);
+                        while !self.is_done() {}
+                        let rb = self.registers.miso().read().bits() as u8;
+                        if let Some(r) = read.get_mut(i) {
+                            *r = rb;
+                        }
+                    }
+                    Ok(())
+                }
+
+                fn transfer_in_place_priv(&mut self, words: &mut [u8]) -> Result<(), Error> {
+                    if self.is_done() {
+                        for word in words.iter_mut() {
+                            self.write_one(word);
+                            while !self.is_done() {}
+                            *word = self.registers.miso().read().bits() as u8;
+                        }
+                        Ok(())
+                    } else {
+                        Err(Error::InvalidState)
                     }
                 }
             }
 
-            impl $crate::hal::blocking::spi::write::Default<u8> for $SPIX {}
-            impl $crate::hal::blocking::spi::transfer::Default<u8> for $SPIX {}
 
-            impl From<$PACSPIX> for $SPIX {
-                fn from(registers: $PACSPIX) -> $SPIX {
-                    $SPIX::new(registers)
+            impl hal::spi::SpiDevice<u8> for $SPIX {
+
+                fn transaction(&mut self, operations: &mut [hal::spi::Operation<'_, u8>]) -> Result<(), Self::Error> {
+
+                    for op in operations {
+                        match op {
+                            hal::spi::Operation::Read(buf) => self.read_priv(buf)?,
+                            hal::spi::Operation::Write(buf) => self.write_priv(buf)?,
+                            hal::spi::Operation::Transfer(read_buf, write_buf) => self.transfer_priv(read_buf, write_buf)?,
+                            hal::spi::Operation::TransferInPlace(buf) => self.transfer_in_place(buf)?,
+                            hal::spi::Operation::DelayNs(_) => continue,
+                        }
+                    }
+
+                    Ok(())
                 }
+
             }
         )+
-    }
+
+    };
 }
+
+// impl hal::spi::SpiBus<u8> for $SPIX {
+
+//     fn flush(&mut self) -> Result<(), Self::Error>{
+//         while !self.is_done() {}
+//         Ok(())
+//     }
+// }
