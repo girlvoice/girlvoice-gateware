efd771e2de7e011a503a69476a669e5ea3fec628
Sage Walker <sage@caesium.dev>
2025-08-08 00:40:40 -0700

    wishbone attached nxlram with working init params

--- girlvoice/platform/nexus_utils/lram.py
+++ girlvoice/platform/nexus_utils/lram.py
@@ -10,7 +10,40 @@ from amaranth_soc.memory import MemoryMap
 
 kB = 1024
 
+def initval_parameters(contents, width=32):
+    """
+    In Radiant, initial values for LRAM are passed a sequence of parameters
+    named INITVAL_00 ... INITVAL_7F. Each parameter value contains 4096 bits
+    of data, encoded as a 1280-digit hexadecimal number, with
+    alternating sequences of 8 bits of padding and 32 bits of real data,
+    making up 64KiB altogether.
+
+    Adapted from Litex
+    """
+    assert width in [32]
+    # Each LRAM is 64KiB == 524288 bits
+    assert len(contents) == 524288 // width, f"{len(contents)} is not formatted for {524288 // width} words"
+    chunk_size = 4096 // width
+    parameters = {}
+    with open("init_params.txt", "a") as f:
+        for i in range(0x80):
+            offset = chunk_size * i
+            if width == 32:
+                value = '0x' + ''.join('00{:08X}'.format(contents[offset + j])
+                                       for j in range(chunk_size - 1, -1, -1))
+            elif width == 64:
+                value = '0x' + ''.join('00{:08X}00{:08X}'.format(contents[offset + j] >> 32, contents[offset + j] | 0xFFFFFF)
+                                       for j in range(chunk_size - 1, -1, -1))
+            parameters['p_INITVAL_{:02X}'.format(i)] = value
+
+            f.write('p_INITVAL_{:02X}={}\n'.format(i, value))
+    return parameters
+
 class NXLRAM(wiring.Component):
+    """
+    Wrapper for the CrosslinkNX Large RAM module
+    """
+
     def __init__(self, init=[]):
         super().__init__({
             "DI": In(32),
@@ -21,14 +54,9 @@ class NXLRAM(wiring.Component):
             "CS": In(1),
             "DO":Out(32)
         })
+        self.params = {}
 
-    @property
-    def size(self):
-        return 512 * kB // 32
-
-    def elaborate(self, platform):
-        m = Module()
-        m.submodules.lram = Instance("SP512K",
+        self.params.update(
             p_ECC_BYTE_SEL = "BYTE_EN",
             i_DI = self.DI,
             i_AD = self.AD,
@@ -40,6 +68,25 @@ class NXLRAM(wiring.Component):
             o_DO = self.DO
         )
 
+        if init != []:
+            self._init = init
+
+    @property
+    def size(self):
+        return 512 * kB // 32
+
+    @property
+    def init(self):
+        return self._init
+
+    @init.setter
+    def init(self, init):
+        self.params.update(initval_parameters(init))
+        self._init = init
+
+    def elaborate(self, platform):
+        m = Module()
+        m.submodules.lram = Instance("SP512K", **self.params)
         return m
 
 class WishboneNXLRAM(wiring.Component):
@@ -48,7 +95,6 @@ class WishboneNXLRAM(wiring.Component):
         granularity = 8
         self.data_width = data_width
         self.size = size
-
 
         if data_width == 32:
             assert size in [64*kB, 128*kB, 192*kB, 256*kB, 320*kB]
@@ -71,6 +117,7 @@ class WishboneNXLRAM(wiring.Component):
         self.wb_bus.memory_map = MemoryMap(addr_width=exact_log2(size), data_width=granularity)
 
         self._lram_blocks = []
+        print(self.depth_cascading)
         for d in range(self.depth_cascading):
             self._lram_blocks.append([])
             for w in range(self.width_cascading):
@@ -80,6 +127,18 @@ class WishboneNXLRAM(wiring.Component):
                 self.wb_bus.memory_map.add_resource(lram_block, name=(f"lram{d}{w}",), size=lram_block.size)
         self.wb_bus.memory_map.freeze()
 
+        if len(init) != 0:
+            self._set_init(init)
+
+
+    def _set_init(self, data: list[int]):
+        data += [0] * ((self.size * 8) // self.data_width - len(data))
+        mem_size_words = 64 * kB * 8 // self.data_width
+        for d in range(self.depth_cascading):
+            for w in range(self.width_cascading):
+                offset = d * self.width_cascading * mem_size_words + w * mem_size_words
+                chunk = data[offset:offset + mem_size_words]
+                self._lram_blocks[d][w].init = chunk
 
     def elaborate(self, platform):
         m = Module()
